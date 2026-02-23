76e1ac2ec0a3eb235e22c3f25d3fbbdf5dd39d95
hgugi3nsn@gmail.com <hgugi3nsn@gmail.com>
2026-02-23 21:06:06 +0700

    dasar variabel lokal

---
+++ tugas pertemuan1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+//variabel global
+int sks;
+
+int main (){
+    //variabel lokal
+    int umur;
+    umur = 21;
+
+    cout << "Halo semua, saya Hugo" << endl;
+    cout << 20 << endl;
+    cout << umur ;
+}
